82c3bb6fb40e8fa33a4171770a93969c7be0741d
shybyte <ein@volloeko.de>
2017-08-10 09:36:49 +0200

    Added PouchDB

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,33 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import PouchDB from 'pouchdb';
+
+const localDB = new PouchDB('kittens');
+const remoteDB = new PouchDB('http://localhost:5984/kittens');
+
+localDB.sync(remoteDB, {
+  live: true,
+  retry: true
+}).on('change', function (change) {
+  console.log('change', change);
+}).on('error', function (err) {
+  console.error(err);
+  console.log(JSON.stringify(err));
+});
+
+function get () {
+  localDB.get('mittens').then(function (doc) {
+    console.log(doc);
+  });
+}
+
 
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
+
+
+get();
