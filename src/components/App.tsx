e0af1ce9fab4c1d547e5e8b2b05dee890043eb7f
shybyte <ein@volloeko.de>
2017-08-11 15:07:53 +0200

    Refactorings, Cleanup

---
+++ src/components/App.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component<{}, {}> {
+  render() {
+    return (
+      <div className="app">
+        <header className="header">
+          <h2>Private Habit Tracker</h2>
+        </header>
+        <main>
+          Content
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
